b5131760730a666d018d9582c8995276fce50035
Majid-Shahani <majid.shahani.1390@gmail.com>
2026-05-01 18:15:51 +0300

    Add grade manager

--- src/main/java/data/GradeManager.java
+++ src/main/java/data/GradeManager.java
@@ -1,4 +1,51 @@
 package data;
 
+import model.GradeRecord;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class GradeManager {
+    private GradeManager() {}
+    private static final DataManager<GradeRecord> s_Grades = new DataManager<>(
+            GradeRecord::fromLine,
+            GradeRecord::toFileString
+    );
+
+    public static void save(Path path) { s_Grades.save(path); }
+    public static void load(Path path) { s_Grades.load(path); }
+
+    public static @Nullable GradeRecord get(String studentUser, String courseCode) {
+        for (var g : s_Grades.get()) if (g.studentUsername().equals(studentUser) && g.courseCode().equals(courseCode))
+            return g;
+        return null;
+    }
+    public static @NotNull ArrayList<GradeRecord> get(String studentUser) {
+        ArrayList<GradeRecord> grades = new ArrayList<>();
+        for (var g : s_Grades.get()) if (g.studentUsername().equals(studentUser)) grades.add(g);
+        return grades;
+    }
+    public static boolean updateGrade(String sUser, String cc, double midterms, double finals) {
+        var it = s_Grades.get().listIterator();
+        while (it.hasNext()) {
+            var g = it.next();
+            if (g.studentUsername().equals(sUser) && g.courseCode().equals(cc)) {
+                it.set(new GradeRecord(sUser, cc, midterms, finals));
+                return true;
+            }
+        }
+        return false;
+    }
+    public static double gpa(String sUser) {
+        double sum = 0;
+        int count = 0;
+        for (var g : s_Grades.get()) if(g.studentUsername().equals(sUser)) {
+            sum += g.calculateAverage();
+            count++;
+        }
+        if (count != 0) return sum / count;
+        else return 0;
+    }
 }
